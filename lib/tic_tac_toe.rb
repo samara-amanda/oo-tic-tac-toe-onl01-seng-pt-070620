c786e2557dc09c865364576ab9b1f16749f040bf
Samara Grant <samara.amanda01@gmail.com>
2020-08-03 02:29:58 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,81 @@
+class TicTacToe
+
+  def initialize
+    board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+    @board = board
+  end
+
+  WIN_COMBINATIONS = [
+  [0,1,2], # top_row
+  [3,4,5], # middle_row
+  [6,7,8], # bottom_row
+  [0,3,6], # left_column
+  [1,4,7], # center_column
+  [2,5,8], # right_column
+  [0,4,8], # left_diagonal
+  [6,4,2] # right_diagonal
+  ]
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+
+  def input_to_index(user_input)
+    user_input.to_i - 1
+  end
+
+  def move(index, current_player)
+    @board[index] = current_player
+  end
+
+  def position_taken?(index)
+   if @board[index] == " " || @board[index] == nil
+      false
+   else
+      true
+   end
+  end
+
+  def valid_move?(index)
+    if index.between?(0,8) && !position_taken?(index)
+      true
+    end
+  end
+
+  def turn_count
+    count = 0
+    @board.each do |spaces|
+      if spaces == "X" || spaces == "O"
+        count += 1
+      end
+    end
+    count
+  end
+
+  def current_player
+    turn_count % 2 == 0 ? "X" : "O"
+  end
+
+  def turn
+    puts "Please enter 1-9:"
+    user_input = gets.strip
+    index = input_to_index(user_input)
+    if valid_move?(index)
+      move(index, current_player)
+      display_board
+    else
+      puts "Please enter 1-9:"
+      user_input = gets.strip
+    end
+  end
+
+
+  def won?
+    WIN_COMBINATIONS.detect do |array|
+      @board[array[0]] == @board[array[1]] && @board[array[1]] == @board[array[2]] && position_taken?(array[0])
+    end
+  end
+end
